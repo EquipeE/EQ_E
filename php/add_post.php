2d2859c0ba683b7cec30a59cc7b96d55c477e446
EquipeE <equipesenae@gmail.com>
2024-09-22 03:22:05 -0300

    Melhorando script de inserção.
    Agora o script php de inserção gera um index.html novo dinamicamente com
    as ultimas postagens. Para isso, ele utiliza o arquivo
    index_template.html na pasta html. Ou seja, não é mais necessário
    alterar o index.html pois o script já faz isso. Quaisquer alterações devem
    ser feitas no template e não no arquivo principal.

--- php/add_post.php
+++ php/add_post.php
@@ -7,7 +7,7 @@ include('db.php');
 
 if (strlen($argv[1]) > MAX_TITLE_LENGTH || strlen($argv[2]) > MAX_IMAGE_PATH_LENGTH)
 	die("Seu titulo ou caminho da imagem são muito longos. O limite é de {MAX_TITLE_LENGTH} caracteres.\n");
-if (!file_exists("../img/{$argv[2]}"))
+if (!file_exists("../img/posts/{$argv[2]}"))
 	die("A imagem {$argv[2]} não existe.\n");
 if (!file_exists($argv[3]))
 	die("O arquivo de conteudo {$argv[3]} não existe.\n");
@@ -24,8 +24,7 @@ $res = $conexao->execute_query("INSERT INTO Posts VALUES (?, ?, ?, ?)", [NULL, $
 if (!$res)
 	die($conexao->error);
 
-/*
-$res = "SELECT * FROM Posts ORDER BY id ASC COUNT 4"->fetch_all(MYSQLI_BOTH);
+$res = $conexao->query("SELECT * FROM Posts ORDER BY id DESC LIMIT 4")->fetch_all(MYSQLI_BOTH);
 
 if (!$res)
 	die($conexao.error);
@@ -34,13 +33,18 @@ $carrosselText = "";
 $cardsText = "";
 
 foreach ($res as $r)
-	$carrosselText += '<div class="slide"><img src=' . './posts/' . $r['imagem'] . ' ></div>' . "\n";
+	$cardsText .= '<div class="card">' . '<a href="./html/post.html?id=' . $r['id'] . '"><img src="' . './img/posts/' . $r['imagem'] . '"></a><div class="card-text">' . $r['titulo'] . '</div></div>' . "\n";
 
-foreach ($res as $)
-	$cardsText += '<div class="card"><img src="' . './posts' . $r['imagem'] . ' ><div class="card-text">' . $r['titulo'] . '</div></div>' . "\n";
+array_pop($res); /* Carrossel usa apenas 3 posts */
 
-echo $carrosselText;
-echo $cardsText;
- */
+foreach ($res as $r)
+	$carrosselText .= '<div class="slide">' . '<a href="./html/post.html?id=' . $r['id'] . '"><img src="' . './img/posts/' . $r['imagem'] . '"></a></div>' . "\n";
+
+$template = file_get_contents("../html/index_template.html");
+$final = str_replace("!!!CARROSSEL", $carrosselText, str_replace("!!!CARDS", $cardsText, $template));
+
+if (!file_put_contents("../index.html", $final))
+	die("Erro gerando index.html");
+
 $conexao->close();
 ?>
--- sql/first_posts.sh
+++ sql/first_posts.sh
@@ -3,4 +3,4 @@
 ../php/add_post.php "História chocante acontece na USP" art2.png ../txt/usp.txt
 ../php/add_post.php "O Monólogo" art3.png ../txt/truth.txt
 ../php/add_post.php "Post 4" art4.png ../txt/ratinho.txt
-../php/add_post.php "Just as the founding fathers intended." logo.png ../txt/founding_fathers.txt
+../php/add_post.php "Just as the founding fathers intended." art1.png ../txt/founding_fathers.txt
