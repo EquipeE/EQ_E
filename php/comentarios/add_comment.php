256ac829ba71f6939d593aec72622df6bcb9672b
EquipeE <equipesenae@gmail.com>
2024-09-25 09:10:55 -0300

    Melhorando o sistema de adicionar comentários.
    Agora o código php/comentarios/add_comment.php verifica mais rigidamente
    se o usuário está ou não logado, evitando até mesmo que ele falsifique
    os cookies manualmente.

--- php/comentarios/add_comment.php
+++ php/comentarios/add_comment.php
@@ -1,11 +1,24 @@
 <?php
-session_start();
-if (!isset($_SESSION['id']))
-	die("Você não está logado.");
 if (!isset($_POST['post_id']) || !isset($_POST['comment']))
 	die("Dados insuficientes.");
 
+session_start();
+
+if (!isset($_SESSION['id']) || !isset($_SESSION['senha']))
+	die("Você não está logado.");
+
 include(__DIR__ . '/../db.php');
+
+$res = $conexao->execute_query("SELECT senha FROM Usuarios WHERE id = ?", [$_SESSION['id']]);
+
+if (!$res)
+	die($conexao->error);
+
+$res = $res->fetch_assoc();
+
+if ($res['senha'] !== $_SESSION['senha'])
+	die("Você não está logado.");
+
 $res = $conexao->execute_query("INSERT INTO Comentarios VALUES (?, ?, ?, ?) ", [NULL, $_POST['post_id'], $_SESSION['id'], $_POST['comment']]);
 
 if (!$res)
