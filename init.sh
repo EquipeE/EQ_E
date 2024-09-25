552cf3a3019d89cef1d57b9bfa83726118066e70
EquipeE <equipesenae@gmail.com>
2024-09-25 09:03:33 -0300

    Fazendo o script init.sh funcionar.
    Como mudei o caminho do script init.sh, ele deixou de funcionar. Corrigi
    os caminhos relativos.

--- init.sh
+++ init.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-chown apache:apache ../ ../html/ ../img/posts/ ../txt # Dando permissão de escrita nas pastas corretas.
+chown apache:apache ./ ./html/ ./img/posts/ ./txt # Dando permissão de escrita nas pastas corretas.
 
-../php/posts/add_post.php "Stallman's Momentary Lapse of Reason" art1.png ../txt/interjection.txt
-../php/posts/add_post.php "História chocante acontece na USP" art2.png ../txt/usp.txt
-../php/posts/add_post.php "O Monólogo" art3.png ../txt/truth.txt
-../php/posts/add_post.php "Post 4" art4.png ../txt/ratinho.txt
-../php/posts/add_post.php "Just as the founding fathers intended." art1.png ../txt/founding_fathers.txt
+./php/posts/add_post.php "Stallman's Momentary Lapse of Reason" art1.png ./txt/interjection.txt
+./php/posts/add_post.php "História chocante acontece na USP" art2.png ./txt/usp.txt
+./php/posts/add_post.php "O Monólogo" art3.png ./txt/truth.txt
+./php/posts/add_post.php "Post 4" art4.png ./txt/ratinho.txt
+./php/posts/add_post.php "Just as the founding fathers intended." art1.png ./txt/founding_fathers.txt
