8b11058011e9a0c32c7e0771ce6fce0f64028461
EquipeE <equipesenae@gmail.com>
2024-09-25 10:41:57 -0300

    Melhorando o login (instável)
    Agora o login exibe uma mensagem de erro ou sucesso. Fiz isso por meio
    duma requisição POST para a própria página. O arquivo html/login.php
    ainda é instável. Posteriormente isso pode ser feito por meio de AJAX e
    o método atual será encaixado dentro duma tag noscript.

--- php/login.php
+++ php/login.php
@@ -1,31 +1,37 @@
 <?php
+function err_wrapper($str) {
+	die("<div id='resultado-erro'><strong>{$str}</strong></div>");
+}
+?>
+
+<?php
 setcookie("test", "test", time() + 120);
 if (count($_COOKIE) === 0)
-	die("Habilite os cookies em seu navegador.");
+	err_wrapper("Habilite os cookies");
 ?>
 
 <?php
 if ($_SERVER['REQUEST_METHOD'] != "POST")
 	die();
 if (!isset($_POST['email']) || !isset($_POST['senha']))
-	die("Preencha todos os campos.\n");
+	err_wrapper("Preencha todos os campos\n");
 
 include_once(__DIR__ . "/db.php");
 
 $res = $conexao->execute_query("SELECT senha, id FROM Usuarios WHERE email = ?", [$_POST['email']])->fetch_assoc();
 
 if (!$res)
-	die("Esse email não está cadastrado.\n");
+	err_wrapper("Email não cadastrado\n");
 if ($res['senha'] != hash('sha256', $_POST['senha']))
-	die("Senha errada.\n");
+	err_wrapper("Senha errada\n");
 
 if (!session_start())
-	die("Erro ao inicializar a sessão.\n");
+	err_wrapper("Erro inicializando sessão\n");
 
 $_SESSION['id'] = $res['id'];
 $_SESSION['senha'] = $res['senha'];
 
-echo "Logado com sucesso.";
+echo "<div id='resultado'><strong>Logado com sucesso</strong></div>";
 
 $conexao->close();
 ?>
