e7b126d02a1842acdc769be2a0e4f58cc4ab4555
EquipeE <equipesenae@gmail.com>
2024-09-30 02:38:44 -0300

    Resolvendo bug pequeno com o botão "Admin".
    Coloquei devolta uma linha que retirei sem querer do arquivo
    php/admin_screen_button.

--- php/admin_screen_button.php
+++ php/admin_screen_button.php
@@ -1,6 +1,8 @@
 <?php
 session_start(["use_strict_mode" => 1, "cookie_httponly" => 1]);
 
+$admin_path = (basename($_SERVER['PHP_SELF']) === 'index.php') ? './html/admin.php' : 'admin.php';
+
 if ($_SESSION['id'] === 1)
 	echo "<li><a href='{$admin_path}'>Admin</a></li>";
 ?>
--- php/login.php
+++ php/login.php
@@ -7,11 +7,11 @@ if ($_SERVER['REQUEST_METHOD'] != "POST")
 
 $success = false;
 
-if (!$cookie_support)
+if (!$cookie_support) 
 	return "Habilite os cookies";
 
 if (!$conexao = new mysqli(DB_SERVER, DB_USER, DB_PASS, DB_NAME))
-	return "Erro ao abrir o banco.";
+	return "Erro ao abrir o banco";
 
 if (!isset($_POST['email']) || !isset($_POST['senha']))
 	return "Preencha todos os campos";
@@ -20,7 +20,7 @@ if (!$res = $conexao->execute_query("SELECT senha, id FROM Usuarios WHERE email 
 	return $conexao->error;
 
 if ($res->num_rows === 0)
-	return "Email não cadastrado";
+	 return "Email não cadastrado";
 
 $res = $res->fetch_assoc();
 
@@ -33,7 +33,7 @@ if (!session_start(["use_strict_mode" => 1, "cookie_httponly" => 1]))
 $_SESSION['id'] = $res['id'];
 
 $success = true;
-return  "Logado com sucesso";
+return "Logado com sucesso";
 
 $conexao->close();
 ?>
